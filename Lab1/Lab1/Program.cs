022c9a20caed9424593bb834ab410dce44476c63
CharithBandara <140784799+CharithBandara@users.noreply.github.com>
2023-07-28 11:23:12 +0530

    Add files via upload

---
+++ Lab1/Lab1/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Program x = new Program();
+            x.question1();
+            x.question2();
+            x.question3();
+            x.question4();
+        }
+
+        void question1()
+        {
+            string name;
+            Console.Write("Enter your name :");
+            name = Console.ReadLine();
+
+            Console.Write("Enter your batch number :");
+            double batch = double.Parse(Console.ReadLine());
+
+            Console.Write(name + " your batch is " + batch);
+
+            Console.ReadKey();
+
+        }
+
+        void question2()
+        {
+            Console.WriteLine("Enter  the radius : ");
+            int r = int.Parse(Console.ReadLine());
+
+            int A = (22 * r * r) / 7;
+            Console.WriteLine("Area is " + A);
+
+            Console.ReadKey();
+
+        }
+
+        void question3()
+        {
+            Console.WriteLine("Enter first number : ");
+            int x = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter second number : ");
+            int y = int.Parse(Console.ReadLine());
+
+            int sum =x + y;
+            Console.WriteLine("Summation is " + sum);
+
+            Console.ReadKey();
+
+        }
+
+        void question4()
+        {
+            Console.WriteLine("Enter your salary in Rupees : ");
+            double sal = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter yoour TAX rate : ");
+            double tax = double.Parse(Console.ReadLine());
+
+            double tot = (sal * (100 - tax)) / 100;
+            Console.WriteLine("Your salary after TAX is Rs." + tot);
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Lab2/Lab2/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Program x = new Program();
+            x.question1();
+            x.question2();
+            x.question3();
+            x.question4();
+            x.question5();
+        }
+
+        void question1()
+        {
+            Console.WriteLine("Enter first number : ");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter second number : ");
+            int b = int.Parse(Console.ReadLine());
+
+            int sum = a + b;
+            Console.WriteLine("Summation is " + sum);
+
+            Console.ReadKey();
+
+        }
+
+        void question2()
+        {
+            Console.WriteLine("Enter first number : ");
+            double x = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter second number : ");
+            double y = double.Parse(Console.ReadLine());
+
+            double sum = x + y;
+            Console.WriteLine("Summation is : " + sum);
+
+            double sub = x - y;
+            Console.WriteLine("Substraction is : " + sub);
+
+            double mul = x * y;
+            Console.WriteLine("Multiplication is : " + mul);
+
+            double div = x / y;
+            Console.WriteLine("Division is : " + div);
+
+            Console.ReadKey();
+
+        }
+
+        void question3()
+        {
+            Console.WriteLine("Enter the radius : ");
+            double r = double.Parse(Console.ReadLine());
+
+            double C = (2 * 22 * r) / 7;
+            double A = (22 * r * r) / 7;
+
+            Console.WriteLine("Circumferance is " + C);
+            Console.WriteLine("Area is " + A);
+
+            Console.ReadKey();
+
+        }
+
+        void question4()
+        {
+            Console.WriteLine("Enter a number : ");
+            double a = double.Parse(Console.ReadLine());
+
+            double no = a % 2;
+            if (no == 0)
+            {
+                Console.WriteLine(a + " is a an even number");
+            }
+            else
+            {
+                Console.WriteLine(a + " is an odd number");
+            }
+
+            Console.ReadKey();
+
+        }
+
+        void question5()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Enter a number : ");
+                double a = double.Parse(Console.ReadLine());
+
+                double no = a % 2;
+                if (no == 0)
+                {
+                    Console.WriteLine(a + " is a an even number");
+                }
+                else
+                {
+                    Console.WriteLine(a + " is an odd number");
+                }
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
